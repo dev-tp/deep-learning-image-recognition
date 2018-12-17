62e0f9592f6351d26aac9e7f13e5e0c027d5ceb6
Eryk <rcksorto@yahoo.com>
2018-12-16 23:40:02 -0800

    Add dense layers to model

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from keras.datasets import cifar10
+from keras.layers import Dense
+from keras.models import Sequential
 from keras.utils import to_categorical
 
 # Load data set
@@ -14,3 +16,20 @@ x_train /= 255
 # Convert class vectors into binary class matrices
 y_test = to_categorical(y_test, 10)
 y_train = to_categorical(y_train, 10)
+
+# Create a model and add layer
+model = Sequential()
+
+# Rectified Linear Units (ReLU) works best with images because it is
+# computationally efficient.
+# The input_shape is (32, 32, 3) because the images are 32x32 and 3 channels
+# for red, green, and blue.
+model.add(Dense(512, activation='relu', input_shape=(32, 32, 3)))
+
+# Since CIFAR-10 data set has 10 different kinds of objects, we create a dense
+# layer with 10 nodes. However, when doing classification with more than one
+# object, the output layer will usually use a softmax activation function.
+model.add(Dense(10, activation='softmax'))
+
+# Print a summary of the model
+model.summary()
